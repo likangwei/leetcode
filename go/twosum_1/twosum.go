438e205256a38411e9dafb11e66ffa4e0cd61422
李康炜 <likangwei@meicai.cn>
2017-10-11 20:51:31 +0800

    format

--- go/twosum_1/twosum.go
+++ go/twosum_1/twosum.go
@@ -4,18 +4,16 @@ import "fmt"
 func twoSum(nums []int, target int) []int {
 	var rst []int
 	m := make(map[int]int)
-	for i := 0; i < len(nums); i++{
-		idx, ok := m[nums[i]]
+	for i, n  := range nums{
+		idx, ok := m[n]
 		if ok{
 			rst = append(rst, idx)
 			rst = append(rst, i)
 			break
 		}else{
-			num_found := target - nums[i]
+			num_found := target - n
 			m[num_found] = i
 		}
-		fmt.Println(m)
-
 	}
 	return rst
 }

f1c41e144705b53c8e50adab5534f733f7401d3e
likangwei <likangwei@meituan.com>
2017-10-12 18:44:28 +0800

    add two num

--- go/add_two_numbers_2/add_two_number.2.go
+++ go/add_two_numbers_2/add_two_number.2.go
@@ -23,15 +23,16 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		sum := extra
 		if l1 != nil{
 			sum = sum + l1.Val
+			l1 = l1.Next
+
 		}
 		if l2 != nil{
 			sum = sum + l2.Val
+			l2 = l2.Next
 		}
 
 		cur.Val = sum % 10
 		extra = sum / 10
-		l1 = l1.Next
-		l2 = l2.Next
 	}
 	return head
 }
@@ -50,5 +51,8 @@ func main() {
 	l1 := ListNode{2, &ListNode{4, &ListNode{3, nil}}}
 	l2 := ListNode{5, &ListNode{6, &ListNode{4, nil}}}
 	rst := addTwoNumbers(&l1, &l2)
+	prettyPrint(rst)
+	l3 := ListNode{5, nil}
+	rst = addTwoNumbers(&l3, &l3)
 	prettyPrint(rst)
 }

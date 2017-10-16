392c0254dfbb9ff86a04425742c36b9436980639
likangwei <likangwei@meituan.com>
2017-10-16 14:32:56 +0800

    提高效率

--- go/zigzag_conversion_6/zigzag_conversion_6.go
+++ go/zigzag_conversion_6/zigzag_conversion_6.go
@@ -2,17 +2,31 @@ package main
 import "fmt"
 import "strconv"
 
+func max(a int, b int) int {  
+    if a > b {  
+        return a  
+    }  
+    return b  
+}  
+
 func convert(s string, numRows int) string {
 	if len(s) < 2 || numRows == 1{
 		return s
 	}
 	var numColumns int
-	numColumns = ((len(s)/(numRows*2-2))+1)*(numRows-1)
-	fmt.Println(numColumns)
-	matrix := make([][]rune, numRows, numColumns)
+	completeNum := len(s) / (numRows*2-2)
+	remainderNum := len(s) % (numRows*2-2)
+	remainderColNum := 0
+	if remainderNum > 0{
+		remainderColNum = 1 + max(0, remainderNum-numRows)
+	} else{
+		remainderColNum = 0
+	}
+	numColumns = completeNum *  (numRows-1) + remainderColNum
+	matrix := make([][]rune, numRows, numRows)
 	i, j := 0, 0
 	isDown := true
-	rst := ""
+	rst := make([]rune, len(s), len(s))
 	for t:=0; t<numRows; t++{
 		matrix[t] = make([]rune, numColumns)
 	}
@@ -31,17 +45,18 @@ func convert(s string, numRows int) string {
 			j = j + 1
 		}
 	}
-	
+	c_idx := 0
 	for i := range matrix{
 		for j:= range matrix[i]{
 			c := matrix[i][j]
 			if c != 0{
-				rst = rst + string(c)
+				rst[c_idx] = c
+				c_idx = c_idx + 1
 			}
 		}
 	}
 
-	return rst
+	return string(rst)
 }
 
 func main() {

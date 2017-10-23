44979ebc39e81d428770c8e1ad1367a2b4a881cf
李康炜 <likangwei@meicai.cn>
2017-10-23 10:14:04 +0800

    14

--- go/longest_common_prefix_14/longest_common_prefix_14.go
+++ go/longest_common_prefix_14/longest_common_prefix_14.go
@@ -11,66 +11,31 @@ https://leetcode.com/problems/longest-common-prefix/description/这个题目做�
 
 package main
 import "fmt"
-
-func isMatch(s string, p string) bool {
-	fmt.Printf("check isMatch(%s, %s) \n", s, p)
-
-	if p == ".*"{
-		return true
+func longestCommonPrefix(strs []string) string {
+	var rst []byte
+	var loop_finish = false
+	if len(strs) == 0 {
+		return ""
 	}
-	if len(p) == 0{
-		return len(s) == 0
-	}
-
-	pc := p[0]
-	atlast_show := 0
-	star_count := 0
-	keep_loop := false
-	var pi, si int = 0, 0
-	for ; pi < len(p); {
-		if p[pi] == '*'{
-			pi += 1
-			star_count += 1
-			keep_loop = true
-		}else if p[pi] == pc{
-			pi += 1
-		}else{
-			break
+	min_str_len := len(strs[0])
+	for _, s := range strs{
+		if min_str_len > len(s){
+			min_str_len = len(s)
 		}
 	}
-	atlast_show = pi - (2 * star_count)
-	// fmt.Printf("%s need %d %c\n", p[:pi], atlast_show, pc)
-	for ;atlast_show !=0 && si < len(s);{
-		if pc == '.' || pc == s[si]{
-			si += 1
-			atlast_show -= 1
-		}else{
-			break
-		}
-	}
-	if atlast_show != 0{
-		return false
-	}
-
-
-	if !keep_loop{
-		return isMatch(s[si:], p[pi:])
-	}else{
-		for ; si < len(s); si++{
-			if isMatch(s[si:], p[pi:]){
-				return true
-			}
-			if s[si] == pc || pc == '.'{
-				// fmt.Println("i need ", s[si+1:], p[pi:])
-				// if isMatch(s[si+1:], p[pi:]){
-				// 	return true
-				// }
-			}else{
+	for i:=0; i < min_str_len && !loop_finish; i++{
+		cur_char := strs[0][i]
+		for _, s := range strs{
+			if cur_char != s[i]{
+				loop_finish = true
 				break
 			}
 		}
-		return isMatch(s[si:], p[pi:])
+		if !loop_finish{
+			rst = append(rst, cur_char)
+		}
 	}
+	return string(rst)
 }
 
 func main() {
@@ -88,7 +53,7 @@ func main() {
 						// "ab", ".*c"
 					}
 	for i := 0; i < len(to_test); i=i+2{
-		fmt.Println(">>>", to_test[i], to_test[i+1], "rst:", isMatch(to_test[i], to_test[i+1]))
+		fmt.Println(">>>", to_test[i], to_test[i+1], "rst:", longestCommonPrefix(to_test))
 	}
 	
 }

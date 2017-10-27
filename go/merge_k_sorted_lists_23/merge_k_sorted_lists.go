0ec2f50a73f197fa3c6836222d530e5f6da7d406
李康炜 <likangwei@meicai.cn>
2017-10-27 10:51:24 +0800

    23

---
+++ go/merge_k_sorted_lists_23/merge_k_sorted_lists.go
@@ -0,0 +1,90 @@
+/*
+https://leetcode.com/problems/merge-k-sorted-lists/description/
+
+总结：
+
+后续优化：
+
+*/
+
+package main
+import "fmt"
+import "math"
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+
+func mergeKLists(lists []*ListNode) *ListNode {
+
+	head := ListNode{}
+	cur := & head
+	var min_idx = -1
+	var min_num = -1
+
+	list_len := len(lists)
+
+	for list_len != 0 {
+		min_num = math.MaxInt64
+		min_idx = -1
+		for idx:=0; idx<list_len;{
+			h := lists[idx]
+			if h == nil{
+				lists[idx] = lists[list_len-1]
+				list_len--
+				continue
+			}
+			if min_num > h.Val{
+				min_idx = idx
+				min_num = h.Val
+			}
+			idx ++
+		}
+		if min_idx != -1{
+			cur.Next = lists[min_idx]
+			cur = cur.Next
+			lists[min_idx] = cur.Next
+		}
+	}
+	return head.Next
+}
+
+
+func buildLinkList(nums[]int) *ListNode {
+	var head ListNode = ListNode{0, nil}
+	var cur_node *ListNode = &head
+	for _, n := range nums{
+		cur_node.Next = &ListNode{n, nil}
+		cur_node = cur_node.Next
+	}
+	return head.Next
+}
+
+func printLinkList(head *ListNode){
+	curNode := head
+	s := ""
+	for ; curNode != nil; curNode=curNode.Next{
+		s += fmt.Sprintf("%d->", curNode.Val)
+	}
+	fmt.Println(s)
+}
+
+
+func main() {
+	to_test := [][]int{
+		[]int{1},
+		[]int{0},
+	}
+	var to_test2 []*ListNode
+	for i:=0; i < len(to_test); i ++{
+		lst := buildLinkList(to_test[i])
+		printLinkList(lst)
+		to_test2 = append(to_test2, lst)
+	}
+
+	rst := mergeKLists(to_test2)
+	printLinkList(rst)
+
+}

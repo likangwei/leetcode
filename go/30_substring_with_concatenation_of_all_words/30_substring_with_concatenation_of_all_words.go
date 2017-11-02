63ddf16cbf4fc4a41225d143b8314c19a6c47fd6
李康炜 <likangwei@meicai.cn>
2017-11-02 10:45:20 +0800

    fix bug

--- go/30_substring_with_concatenation_of_all_words/30_substring_with_concatenation_of_all_words.go
+++ go/30_substring_with_concatenation_of_all_words/30_substring_with_concatenation_of_all_words.go
@@ -27,11 +27,9 @@ func buildCombinationMap(words []string) *Map{
 	childs := make(map[string]*Map)
 	rst := Map{&childs}
 	for idx, s := range words{
-		fmt.Println("words", words)
-		left_words := []string{}
-		left_words = append(left_words, words[0:idx]...)
+		left_words := words[0:idx]
 		left_words = append(left_words, words[idx+1:len(words)]...)
-		fmt.Println(words, idx, "left", left_words)
+		// fmt.Println(words, idx, "left", left_words)
 		childs[s] = buildCombinationMap(left_words)
 	}
 	return &rst

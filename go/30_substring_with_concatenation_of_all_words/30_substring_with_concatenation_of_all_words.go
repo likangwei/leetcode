8b7aaa8988bc87b3186aa7bfb799e81a05621f0b
李康炜 <likangwei@meicai.cn>
2017-11-02 21:04:01 +0800

    30

--- go/30_substring_with_concatenation_of_all_words/30_substring_with_concatenation_of_all_words.go
+++ go/30_substring_with_concatenation_of_all_words/30_substring_with_concatenation_of_all_words.go
@@ -9,9 +9,79 @@ import "fmt"
 /*
 https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/
 
-高手答案
+我的结果：
+  1. 第一次耗时非常长， 用内存也多
+  2. 第二次优化完，accept
 
-总结：
+当前水平改进：
+  1. 为什么第一次耗时长，内存多？ 因为我喜欢遍历所有情况，将所有可能出现的情景都复现，有好多次这种操作了
+     
+
+
+高手答案
+func skip(s string, start, k int, word string, occurence map[string]int) int {
+	var i int
+	for i = start; i <= len(s)-k; i += k {
+		w := s[i : i+k]
+		occurence[w]--
+		if w == word {
+			break
+		}
+	}
+	return i + k
+}
+
+func clearOccurence(occurence map[string]int) {
+	for word, _ := range occurence {
+		occurence[word] = 0
+	}
+}
+
+func find(s string, offset, k, total int, tbl map[string]int) []int {
+	start := -1
+	res := make([]int, 0)
+	occurence := make(map[string]int)
+	for i := 0; i <= len(s)-k; i += k {
+		word := s[i : i+k]
+		if _, ok := tbl[word]; !ok {
+			start = -1
+			continue
+		}
+		if start == -1 {
+			start = i
+			clearOccurence(occurence)
+		}
+		occurence[word] += 1
+		if occurence[word] > tbl[word] {
+			start = skip(s, start, k, word, occurence)
+		}
+		if i+k-start == total {
+			res = append(res, start+offset)
+		}
+	}
+	return res
+}
+
+func findSubstring(s string, words []string) []int {
+	if len(s) == 0 || len(words) == 0 {
+		return []int{}
+	}
+	k := len(words[0])
+	if len(s) < k {
+		return []int{}
+	}
+	tbl := make(map[string]int)
+	for _, word := range words {
+		tbl[word] += 1
+	}
+	total := len(words) * k
+	ans := make([]int, 0)
+	for i := 0; i < k; i++ {
+		res := find(s[i:], i, k, total, tbl)
+		ans = append(ans, res...)
+	}
+	return ans
+}
 
 */
 
@@ -30,7 +100,6 @@ func buildCombinationMap(words []string) *Map{
 		left_words := []string{}
 		left_words = append(left_words, words[0:idx]...)
 		left_words = append(left_words, words[idx+1:len(words)]...)
-		fmt.Println(words, idx, "left", left_words)
 		childs[s] = buildCombinationMap(left_words)
 	}
 	return &rst
@@ -84,17 +153,74 @@ func findSubstring(s string, words []string) []int {
 	return rst
 }
 
+
+func findSubstring2(s string, words []string) []int {
+	rst := []int{}
+	if len(words) == 0 || len(words[0]) == 0{
+		return []int{0}
+	}
+	if len(s)==0{
+		return []int{}
+	}
+	len_total := len(words[0]) * len(words)
+	skip := len(words[0])
+	if len_total > len(s){
+		return []int{}
+	}
+	m := make(map[string]int)
+	for _, s := range words{
+		v, ok := m[s]
+		if ok{
+			m[s] = v + 1
+		}else{
+			m[s] = 1
+		}
+	}
+	for i:=0; i <= len(s) - len_total; i++{
+		curMap := make(map[string]int)
+		found := true
+		for j:=i; j<i+len_total; j+=skip{
+			curStr := s[j:j+skip]
+			v, ok := m[curStr]
+			if ok{
+				c, ok := curMap[curStr]
+				if ok{
+					c = c + 1
+				}else{
+					c = 1
+				}
+				curMap[curStr] = c
+				if c > v{
+					found = false
+					break
+				}
+
+			}else{
+				found = false
+				break
+			}
+		}
+		if found{
+			rst = append(rst, i)
+		}
+	}
+	return rst
+}
+
+
 func main() {
 	to_test := []string{
 		"barfoothefoobarman",
+		"pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel",
 	}
 
 	param2 := [][]string{
 		[]string{"foo", "bar"},
+		[]string{"dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"},
 	}
 
 	for i:=0; i < len(to_test); i+=1{
-		rst := findSubstring(to_test[i], param2[i])
+		rst := findSubstring2(to_test[i], param2[i])
 		fmt.Println(to_test[i], param2[i], rst)
 	}
 }

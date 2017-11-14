7a8e5221a5a28c4528ae74c13176aa8e0f057275
likangwei <likangwei@meituan.com>
2017-11-14 13:54:23 +0800

    43

--- go/043_multiply_strings/43_multiply_strings.go
+++ go/043_multiply_strings/43_multiply_strings.go
@@ -22,41 +22,86 @@ https://leetcode.com/problems/valid-sudoku/description/
 
 */
 
+func getInteger(lst []byte)int{
+	rst, prod := 0, 1
+	for i:=len(lst)-1; i>=0; i--{
+		rst = rst + (prod * int(lst[i]-'0'))
+		prod *= 10
+	}
+	return rst
+}
+
+func multiplyLst(longLst []byte, shortLst []byte)[]byte{
+	if len(longLst) < len(shortLst){
+		longLst, shortLst = shortLst, longLst
+	}
+	rst := []byte{}
+	if len(longLst) + len(shortLst) > 18{
+		for i:=len(longLst)-1; i>=0; i--{
+			bts := multiplyLst(shortLst, []byte{longLst[i]})
+			for j:=0; j<(len(longLst)-i-1); j++{
+				bts = append(bts, '0')
+			}
+			k, l := len(bts)-1, len(rst)-1
+			nlen := len(bts) + 1
+			if len(bts) < len(rst){ nlen = len(rst) + 1}
+			newRst := make([]byte, nlen, nlen)
+			rstIdx := nlen - 1
+			buf := 0
+			for ; k>=0 || l>=0 || buf > 0;{
+				b := byte('0')
+				if k >= 0{
+					b = b + bts[k] - '0'
+					k--
+				}
+				if l >= 0{
+					b = b + rst[l] - '0'
+					l--
+				}
+				if buf > 0{
+					b = b + byte(buf)
+					buf = 0
+				}
+				if b > '9'{
+					b, buf = b - 10, 1
+				}
+				newRst[rstIdx] = b
+				rstIdx --
+			}
+			if newRst[0] == 0{
+				newRst = newRst[1:len(newRst)]
+			}
+			rst = newRst
+		}
+		return rst
+	}else{
+		rstNum := getInteger(longLst) * getInteger(shortLst)
+		if rstNum == 0{
+			return []byte{'0'}
+		}
+		for rstNum > 0{
+			rst = append([]byte{'0' + byte(rstNum % 10)}, rst...)
+			rstNum = rstNum / 10
+		}
+		return rst
+	}
+}
+
+
 func multiply(num1 string, num2 string) string {
-	longNum, shortNum, longIdx, shortIdx := num1, num2, len(num1)-1, len(num2)-1
-	if longIdx < shortIdx{
-		longNum, shortNum, longIdx, shortIdx = num2, num1, shortIdx, longIdx
+	longNum, shortNum := num1, num2
+	if len(longNum) < len(shortNum){
+		longNum, shortNum = num2, num1
 	}
-	rst := make([]byte, len(num1)+len(num2), len(num1)+len(num2))
-	rstLen := 0
-	buf := 0
-	for shortIdx >= 0{
-		b := longNum[longIdx] + (shortNum[shortIdx] - '0') + byte(buf)
-		buf = 0
-		if b > '9'{
-			b, buf = b - 10, 1
-		}
-		rst[rstLen] = b
-		rstLen ++
-		shortIdx -- 
-		longIdx --
+	longBts := make([]byte, len(longNum), len(longNum))
+	for i, b :=  range longNum{
+		longBts[i] = byte(b)
 	}
-	for longIdx >= 0{
-		b := longNum[longIdx] + byte(buf)
-		buf = 0
-		if b > '9'{
-			b, buf = b - 10, 1
-		}
-		rst[rstLen] = b
-		rstLen ++
-		longIdx --
+	shortBts := make([]byte, len(shortNum), len(shortNum))
+	for i, b := range shortNum{
+		shortBts[i] = byte(b)
 	}
-	if buf == 1{
-		rst[rstLen] = '0' + byte(buf)
-		rstLen ++
-	}
-	rst = rst[:rstLen]
-	return string(rst)
+	return string(multiplyLst(longBts, shortBts))
 }
 
 func main() {
@@ -73,7 +118,9 @@ func main() {
 	// }
 
 	to_test := [][]string{
-		[]string{"123", "999"},
+		[]string{"0", "0"},
+		[]string{"1234", "999"},
+		[]string{"9223372036854775808", "9223372036854775808"},
 	}
 
 	// to_test := [][][]byte{

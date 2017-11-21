bb3a2090755eaa45760b4f6de18d2194bfdacc1f
李康炜 <likangwei@meicai.cn>
2017-11-21 11:10:23 +0800

    f

---
+++ go/051_n_queens/051_n_queens.go
@@ -0,0 +1,31 @@
+
+package main
+import "fmt"
+/*
+
+https://leetcode.com/problems/n-queens/description/
+
+高手答案:  
+
+1. vs高手
+    * 时间
+    * 空间、变量个数
+    * 行数
+    * 命名、可读性
+    * 技巧
+    * 是否有递归
+2. 此题感悟
+
+
+*/
+
+func main() {
+	to_test = []int{}
+
+	for i:=0; i < len(to_test); i++{
+		p1 := to_test[i]
+		rst := combinationSum2(p1, to_test2[i])
+		fmt.Println(p1, rst)
+	}
+
+}

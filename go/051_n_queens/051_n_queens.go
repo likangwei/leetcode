a5ac98657dd6ea85470bb748fa60b977ee201691
likangwei <likangwei@meituan.com>
2017-11-22 13:45:09 +0800

    51

--- go/051_n_queens/051_n_queens.go
+++ go/051_n_queens/051_n_queens.go
@@ -5,6 +5,9 @@ import "fmt"
 
 https://leetcode.com/problems/n-queens/description/
 
+此题发挥不错，13ms。 大于了 75%的 commiter
+
+
 高手答案:  
 
 1. vs高手
@@ -19,13 +22,64 @@ https://leetcode.com/problems/n-queens/description/
 
 */
 
+
+func buildNQueue(buf [][]int , curStep int , n int, rst (*[][]string)){
+	// fmt.Println(buf, curStep, n, *rst)
+	if curStep == n{
+		board := make([]string, n)
+		for i, row := range buf{
+			bts := make([]byte, n)
+			for j:=0; j<n; j++{
+				bts[j] = '.'
+			}
+			bts[row[1]] = 'Q'
+			board[i] = string(bts)
+		}
+		*rst = append(*rst, board)
+		return
+	}
+	solts := make([]bool, n)
+	for pIdx:=0; pIdx<curStep; pIdx++{
+		point := buf[pIdx]
+		solts[point[1]] = true
+		dis := curStep - pIdx
+		left, right := point[1] - dis, point[1] + dis
+		if left >= 0 && left < n{
+			solts[left] = true
+		}
+		if right >= 0 && right < n{
+			solts[right] = true
+		}
+	}
+	for i, b := range solts{
+		if !b{
+			buf[curStep] = []int{curStep, i}
+			buildNQueue(buf, curStep+1, n, rst)
+		}
+	}
+}
+
+func solveNQueens(n int) [][]string {
+    buf := make([][]int, n)
+    rst := &([][]string{})
+    buildNQueue(buf, 0, n, rst)
+    return *rst
+}
+
 func main() {
-	to_test = []int{}
+	to_test := []int{
+		4, 
+	}
 
 	for i:=0; i < len(to_test); i++{
 		p1 := to_test[i]
-		rst := combinationSum2(p1, to_test2[i])
-		fmt.Println(p1, rst)
+		rst := solveNQueens(p1)
+		for _, queen := range rst{
+			for _, row := range queen{
+				fmt.Println(row)
+			}
+			fmt.Println("\n")
+		}
+		
 	}
-
 }

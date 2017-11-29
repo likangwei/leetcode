4a5ada5ec9bef8019c70fa8663f7119d8a8afe6d
李康炜 <likangwei@meicai.cn>
2017-11-29 09:54:18 +0800

    f

--- go/045_jump_game_ii/045_jump_game_ii.go
+++ go/045_jump_game_ii/045_jump_game_ii.go
@@ -8,12 +8,12 @@ https://leetcode.com/problems/jump-game-ii/description/
 高手答案:  
 
 1. vs高手
-    * 时间
-    * 空间、变量个数
-    * 行数
-    * 命名、可读性
-    * 技巧
-    * 是否有递归
+	* 时间
+	* 空间、变量个数
+	* 行数
+	* 命名、可读性
+	* 技巧
+	* 是否有递归
 2. 此题感悟
 
 此次做的差的地方:
@@ -25,12 +25,58 @@ https://leetcode.com/problems/jump-game-ii/description/
 后续如何改进:
 */
 
+func jump(nums []int) int {
+	rst := 0
+	lastIdx := len(nums) - 1
+	for lastIdx != 0{
+		for i:=0; i<lastIdx; i++{
+			if nums[i] + i >= lastIdx{
+				lastIdx = i
+				rst ++
+				break
+			}
+		}
+	}
+	return rst
+}
+
+//master
+func jump_master(nums []int) int {
+   // BFS
+    if len(nums) < 2 {
+        return 0
+    }
+    currentMax, nextMax, level, i := 0, 0, 0, 0
+    
+    for i <= currentMax {
+        level ++
+        for ; i<=currentMax; i++{
+            nextMax = max(nums[i]+i, nextMax)
+            if nextMax >=len(nums) -1{
+                return level
+            }
+        }
+        currentMax = nextMax
+    }
+    return 0
+}
+
+func max(a, b int) int{
+    if a < b {
+        return b
+    }
+    return a
+}
+
+
 func main() {
-	to_test = []int{}
+	to_test := [][]int{
+		[]int{2,3,1,1,4},
+	}
 
 	for i:=0; i < len(to_test); i++{
 		p1 := to_test[i]
-		rst := combinationSum2(p1, to_test2[i])
+		rst := jump(p1)
 		fmt.Println(p1, rst)
 	}
 

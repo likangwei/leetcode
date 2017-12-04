afd2c3969e56bca776b3ff69fdfe545e77b122b8
李康炜 <likangwei@meicai.cn>
2017-12-04 10:38:23 +0800

    f

---
+++ go/329_longest_increasing_path_in_a_matrix/329_longest_increasing_path_in_a_matrix.go
@@ -0,0 +1,72 @@
+
+package main
+import "fmt"
+/*
+
+https://leetcode.com/problems/longest_increasing_path_in_a_matrix/description/
+
+description:
+
+# 速度类
+
+Q: 此次的时间消耗是？ vs 专家？
+A: 
+
+Q: 此题目的算法复杂度是？
+A:
+
+# 可读性
+Q: 此次的算法精炼度是怎么样的？
+A:
+
+Q: 此次的命名、可读性怎么样？
+A:
+
+# 思路类
+Q: 此次是否有你递归，专家没递归的情况？
+A: 
+
+Q: 为什么你没有想到专家思路？
+A:
+
+# 后续改进
+Q: 此次做的差的地方?
+A: 
+
+Q: 此次发挥好的地方?
+A:
+
+
+Q: 之前的总结是否生效？这次没用上的原因是？以后如何保证能用上？
+A:
+
+Q: 后续如何改进?
+A:
+
+Q: 此问题是否可以有实际应用场景？
+A:
+
+Q: 此题目对应的TAG是topological-sort, depth-first-search, memoization, 此TAG解决了哪类问题？
+A: 
+
+
+3. TODO:
+
+*/
+
+//Expert answer
+
+func reProduction(){
+
+}
+
+func main() {
+	to_test = []int{}
+
+	for i:=0; i < len(to_test); i++{
+		p1 := to_test[i]
+		rst := combinationSum2(p1, to_test2[i])
+		fmt.Println(p1, rst)
+	}
+
+}

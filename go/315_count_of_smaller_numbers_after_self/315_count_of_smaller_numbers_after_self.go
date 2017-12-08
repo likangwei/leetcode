189683bbc159010442250a5d47657c4fda72eddc
likangwei <likangwei@meituan.com>
2017-12-08 13:14:04 +0800

    finish 315

--- go/315_count_of_smaller_numbers_after_self/315_count_of_smaller_numbers_after_self.go
+++ go/315_count_of_smaller_numbers_after_self/315_count_of_smaller_numbers_after_self.go
@@ -59,7 +59,7 @@ Q: 此次的时间消耗是？ vs 专家？
 A: 1st: 1116ms https://leetcode.com/submissions/detail/130996108/
 
 Q: 此题目的算法复杂度是？
-A: O(n)
+A: 空间复杂度为O(n log n)，查询的时间复杂度则为 O(log n+k)}
 
 # 可读性
 Q: 此次的算法精炼度是怎么样的？
@@ -98,11 +98,25 @@ A: 暂时没想到
 
 Q: 此题目对应的TAG是segment-tree, divide-and-conquer,
                   binary-indexed-tree, binary-search-tree, 此TAG解决了哪类问题？
-A: 
- 
+A: segment-tree  线段树， 每一个节点代表一个区间
+   divide-and-conquer 划分和占领  
+     字面上的解释是“分而治之”，就是把一个复杂的问题分成两个或更多的
+     相同或相似的子问题，直到最后子问题可以简单的直接求解，原问题的
+     解即子问题的解的合并。
+   binary-indexed-tree 二分索引树
+     现多用于高效计算数列的前缀和， 区间和。它可以以 O(log n)的时间得到任意前缀和
+     并同时支持在 O(log n) O(log n)时间内支持动态单点值的修改。空间复杂度  O(n)
+   binary-search-tree 二分查找树
+     二叉查找树相比于其他数据结构的优势在于查找、插入的时间复杂度较低。为O(log n)
+     二叉查找树是基础性数据结构，用于构建更为抽象的数据结构，如集合、multiset、关联数组等。
+
 Q: 你觉得专家答案有什么值得你学习的地方？ 后续如何保证能用到，或者提高用到的概率？
+   专家用到BST做到了部分统计，而我当时想一下全部统计出来，比如
+   6的结果是<5的个数和 5的个数，而专家的思想是只在左边挂sum，而右边
+   是可以求出来的
 
 Q: 对于日常生活中，专家的思维方式给我带来了哪些启发？
+   少量优化也是可以优化的。
 
 最终总结，并加入复习列表:
  url: https://leetcode.com/submissions/detail/130996108/
